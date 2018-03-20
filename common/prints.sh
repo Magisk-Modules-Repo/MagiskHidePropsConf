8e49ffa45e17b63f7052aaf21adb13a28d91f3c1
Didgeridoohan <Didgeridoohan@users.noreply.github.com>
2018-03-20 19:03:29 +0100

    List version and new print

--- common/prints.sh
+++ common/prints.sh
@@ -3,6 +3,9 @@
 # MagiskHide Props Config
 # By Didgeridoohan @ XDA-Developers
 
+PRINTSV=1
+PRINTSTRANSF=1
+
 PRINTSFILE=/sdcard/printslist
 
 # Certified fingerprints
@@ -12,6 +15,7 @@ Google Pixel 2 XL=google/taimen/taimen:8.1.0/OPM1.171019.013/4474084:user/releas
 OnePlus 3T=OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR6.170623.013/12041042:user/release-keys
 OnePlus 5T=OnePlus/OnePlus5T/OnePlus5T:7.1.1/NMF26X/12152312:user/release-keys
 Samsung S8 Plus=samsung/dream2ltexx/dream2lte:7.0/NRD90M/G955FXXU1AQGB:user/release-keys
+Sony Xperia Z3=Sony/D6633/D6633:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 Xiaomi Mi6=Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
 "
 
